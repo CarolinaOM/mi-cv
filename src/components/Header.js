8ddb0f9509f16954923771eff8cc1825d57fad05
CarolinaOM <carolina13081991@gmail.com>
2025-08-14 18:18:19 +0100

    empezando cv se puso nombre y datos de contacto

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import './Header.css';
+import { FaEnvelope, FaMapMarkerAlt, FaLinkedin, FaGithub } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header className="header-container">
+      <div className="header-left">
+        <h1>Carolina Ordoñez</h1>
+        <h2>Desarrolladora Frontend</h2>
+      </div>
+
+      <div className="header-right">
+        <p>
+          <FaEnvelope className="icon email" />
+          carolinaisabelom@gmail.com
+        </p>
+        <p>
+          <FaMapMarkerAlt className="icon location" />
+          Tenerife, España
+        </p>
+        <p>
+          <FaLinkedin className="icon linkedin" />
+          <a
+            href="https://www.linkedin.com/in/carolina-ordonez-morales"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
+        <p>
+          <FaGithub className="icon github" />
+          <a
+            href="https://github.com/CarolinaOM"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
